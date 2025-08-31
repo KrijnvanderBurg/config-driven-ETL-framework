213d5c6fa385520341f6ef6f9dd2e1ff49528425
Krijn van der Burg <krijnvdburg@protonmail.com>
2025-08-31 09:34:37 +0000

    w

--- src/flint/job/models/validate.py
+++ src/flint/job/models/validate.py
@@ -10,10 +10,12 @@ Available Validators:
 
 import logging
 from abc import ABC, abstractmethod
-from typing import Generic, TypeVar
+from typing import TYPE_CHECKING, Generic, TypeVar
 
-from flint.job.core.job import Job
 from flint.utils.logger import get_logger
+
+if TYPE_CHECKING:
+    from flint.job.core.job import Job
 
 logger: logging.Logger = get_logger(__name__)
 
