f48c5b8e866077cb2a2903c9b205d93b3c09271e
Krijn van der Burg <krijnvdburg@protonmail.com>
2025-10-12 21:54:52 +0200

    w

--- scripts/setup-poetry.sh
+++ scripts/setup-poetry.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 target_path="${1:-$PWD}" && echo "Target path: $target_path"
 
-# Install Poetry if not already installed
+# Install Poetry via pipx if not already installed
 if ! command -v poetry &> /dev/null; then
-    echo "Installing Poetry..."
-    curl -sSL https://install.python-poetry.org | python3 -
+    echo "Installing Poetry via pipx..."
+    python -m pip install --user pipx
+    python -m pipx ensurepath
+    pipx install poetry
 fi
 echo -n "Poetry version: " && poetry --version
+
+# Configure Poetry to not create virtualenvs
+echo "Configuring Poetry..."
+poetry config virtualenvs.create false
+poetry config virtualenvs.in-project false
 
 # Update lock file
 echo "Updating lock file in $target_path"
