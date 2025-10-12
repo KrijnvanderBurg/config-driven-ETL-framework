3b8c6d66237933a4154967fd70e2484d7a0365e5
Krijn van der Burg <krijnvdburg@protonmail.com>
2025-10-12 21:02:30 +0000

    w

--- scripts/run-semgrep.sh
+++ scripts/run-semgrep.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 target_path="${1:-$PWD/src}" && echo "Scanning folder: $target_path"
 config_filepath="${2:-$PWD/config/semgrep.yaml}" && echo "Config file: $config_filepath"
-junit_output="${3:-$PWD/semgrep-junit.xml}" && echo "JUnit output: $junit_output"
-sarif_output="${4:-$PWD/semgrep.sarif}" && echo "SARIF output: $sarif_output"
+sarif_output="${3:-$PWD/semgrep.sarif}" && echo "SARIF output: $sarif_output"
 
 # Install semgrep if not already installed
 if ! command -v semgrep &> /dev/null; then
@@ -12,13 +11,12 @@ fi
 
 # add to run custom rules
 # --config "$config_filepath" \
+
 semgrep scan "$target_path" \
   --config "p/default" \
   --config "p/python" \
-  --junit-xml \
-  -o "$junit_output" \
   --sarif \
-  --sarif-output="$sarif_output" \
+  -o "$sarif_output" \
   --error \
   --text \
   --no-autofix \
